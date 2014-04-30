e33cf23b76839352a94fe23f7afbaf81c971edee
fkbyf <a462611226@gmail.com>
2014-04-30 19:15:48 +0400

    Граф/эхэх2.2

--- src/org/alina/stepic/UndirectedGraph.java
+++ src/org/alina/stepic/UndirectedGraph.java
@@ -15,15 +15,20 @@ public class UndirectedGraph {
 
 	static class Node {
 		int flag = 0;
+		int id;
 		ArrayList<Node> friends = new ArrayList();
 
 		public void add(Node a) {
 			friends.add(a);
 		}
 
-		boolean containsNode(Node n) {
-			return friends.contains(n);
+		public Node(int i) {
+			id = i;
 		}
+
+		/*boolean containsNode(Node n) {
+			return friends.contains(n);
+		}*/
 	}
 
 	static void DFS(int x) {
@@ -36,33 +41,41 @@ public class UndirectedGraph {
 			System.out.println("True");
 			isAgreed = true;
 		}
-		for (int z = 0; z < nodes[x].friends.size(); z++) {
+		for (Node z : nodes[x].friends) {
+			if (z.flag != 2) {
+				DFS(z.id);
+			}
+		}
+		/*for (int z = 0; z < nodes[x].friends.size(); z++) {
 			if (nodes[z].flag != 2 && nodes[x].containsNode(nodes[z])) {
 				DFS(z);
-			}
-			if (isAgreed) {
-				return;
-			}
-			nodes[x].flag = 2;
+			}*/
+		if (isAgreed) {
+			return;
 		}
-		if (x == UndirectedGraph.x && !isAgreed)
+		nodes[x].flag = 2;
+		if(x == UndirectedGraph.x&&!isAgreed){
+
 			System.out.println("False");
+		}
 	}
+
+
 
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		nodes = new Node[s.nextInt()];
 		for (int i = 0; i < nodes.length; i++) {
-			nodes[i] = new Node();
+			nodes[i] = new Node(i);
 		}
 
 		int m = s.nextInt();  //количество рёбер
 		while (m-- > 0) {
 			int i = s.nextInt() - 1;
 			int j = s.nextInt() - 1;
-			nodes[i ].add(nodes[j ]);
-			nodes[j ].add(nodes[i ]);
+			nodes[i].add(nodes[j]);
+			nodes[j].add(nodes[i]);
 
 		}
 		x = s.nextInt();
